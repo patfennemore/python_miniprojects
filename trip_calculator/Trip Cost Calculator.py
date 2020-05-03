2523e49afaa702fe864a3e39ee89a2fc9e7691e7
Mantap Apa <patfennemore@gmail.com>
2020-05-03 14:09:29 +0700

    Trip Cost Calculator Project

---
+++ trip_calculator/Trip Cost Calculator.py
@@ -0,0 +1,10 @@
+
+drive_distance = input("How many kilometres will you drive? ")
+fuel_usage = input("How many litres per km does your car use? ")
+price_fuel = input("What is the current price of fuel per litre? (eg. 10 USD) ")
+
+split_currency_amount = price_fuel.split()
+
+trip_cost = ((float(drive_distance)*float(fuel_usage))*float(split_currency_amount[0]))
+
+print(str(trip_cost) + " " + split_currency_amount[1])
