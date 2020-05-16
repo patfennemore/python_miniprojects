a70a260f218987f3da96abf0b579363a48d01423
Mantap Apa <patfennemore@gmail.com>
2020-05-16 10:17:40 +0700

    Added library and finished function determine_winner

--- rps_game/rps_game.py
+++ rps_game/rps_game.py
@@ -1,37 +1,37 @@
-dict_rps = {"Rock": 0, "Paper": 1, "Scissors": 2}
-user_hand = int(input("Please enter a number between 0-2: "))
+count = 0
+while count == 0:
+    dict_rps = {"Rock": 0, "Paper": 1, "Scissors": 2}
+    user_hand = int(input("Please enter a number between 0-2: "))
 
-import random
+    import random
 
-computer_hand = random.randrange(0, 3)
-print(computer_hand)
-
-
-def get_hand(number):
-    for rps, index in dict_rps.items():
-        if number == index:
-            dict_rps[rps] = index
-            print(rps)
-    return number
+    computer_hand = random.randrange(0, 3)
 
 
-get_hand(user_hand)
-get_hand(computer_hand)
-
-computer_player = False
-user_player = True
-
-def determine_winner(hand):
-
+    def get_hand(number):
+        for rps, index in dict_rps.items():
+            if number == index:
+                dict_rps[rps] = index
+                print(rps)
+        return number
 
 
-# determine_winner(computer_hand, user_hand)
-# * print out the player hand and computer hand
-# * print out the winner
-#
-# ## Some Tips
-#
-# Creating a function that gets a "hand" based on a number.
-#
-# The function should take in a number and return the string representation of the hand. E.g.:
-#
+    print("Your hand is: ", end='')
+    get_hand(user_hand)
+    print("Computer's hand is: ", end='')
+    get_hand(computer_hand)
+
+    score = computer_hand - user_hand
+
+
+    def determine_winner(result):
+        if score == 0:
+            print("It is a Draw \n")
+        elif score == (1, -2):
+            print("You Win \n")
+        else:
+            print("The computer wins \n")
+        return result
+
+
+    determine_winner(score)
